fc0b4d4e422be36548a9519042dd9ee6a390c4cd
SingularDevice <thesingulardevice@gmail.com>
2024-05-04 21:54:43 +0200

    feat: Implement swagger dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  configureSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
+
+const configureSwagger = (app: INestApplication): void => {
+  const config: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API endpoints documentation')
+    .setVersion('1.0')
+    .build();
+
+  const api: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, api);
+};
